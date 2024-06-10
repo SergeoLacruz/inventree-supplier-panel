998a39ee3136c577efabc8762e3a39bf2d30151d
Michael <michael@buchmann.ruhr>
2024-06-10 13:33:15 +0200

    Moved mouser stuff to a separate class

---
+++ inventree_supplier_panel/mouser.py
@@ -0,0 +1,146 @@
+from common.models import InvenTreeSetting
+
+from inventree_supplier_panel.request_wrappers import Wrappers
+from inventree_supplier_panel.meta_access import MetaAccess
+import re
+import json
+
+
+class Mouser():
+
+    # --------------------------- get_mouser_partdata -----------------------------
+    def get_mouser_partdata(self, sku):
+
+        part_data = {}
+        part = {"SearchByPartRequest": {"mouserPartNumber": sku,
+                                        "partSearchOptions": "exact"
+                                        }
+                }
+        url = 'https://api.mouser.com/api/v1.0/search/partnumber?apiKey=' + self.get_setting('MOUSERSEARCHKEY')
+        header = {'Content-type': 'application/json', 'Accept': 'application/json'}
+        response = Wrappers.post_request(self, json.dumps(part), url, header)
+        if response.status_code != 200:
+            return (part_data)
+        response = response.json()
+        if response['Errors'] != []:
+            self.status_code = 'Error, '
+            self.message = response['Errors']
+            return (part_data)
+        number_of_results = int(response['SearchResults']['NumberOfResult'])
+        if number_of_results == 0:
+            self.status_code = 'Error, '
+            self.message = 'Part not found: ' + sku
+            return (part_data)
+        if number_of_results > 1:
+            self.status_code = 'Error, '
+            self.message = 'Multiple parts found. Check supplier part number: ' + sku
+            return (part_data)
+        part_data['SKU'] = response['SearchResults']['Parts'][0]['MouserPartNumber']
+        part_data['MPN'] = response['SearchResults']['Parts'][0]['ManufacturerPartNumber']
+        part_data['URL'] = response['SearchResults']['Parts'][0]['ProductDetailUrl']
+        part_data['lifecycle_status'] = response['SearchResults']['Parts'][0]['LifecycleStatus']
+        part_data['pack_quantity'] = response['SearchResults']['Parts'][0]['Mult']
+        part_data['description'] = response['SearchResults']['Parts'][0]['Description']
+        part_data['package'] = Mouser.get_mouser_package(self, response['SearchResults']['Parts'][0])
+        part_data['price_breaks'] = []
+        for pb in response['SearchResults']['Parts'][0]['PriceBreaks']:
+            new_price = Mouser.reformat_mouser_price(self, pb['Price'])
+            part_data['price_breaks'].append({'Quantity': pb['Quantity'], 'Price': new_price, 'Currency': pb['Currency']})
+        self.status_code = 200
+        self.message = 'OK'
+        return (part_data)
+
+    # ------------------------------- get_mouser_package --------------------------
+    # Extracts the available packages from the Mouser part data json
+    def get_mouser_package(self, part_data):
+        package = ''
+        try:
+            attributes = part_data['ProductAttributes']
+        except Exception:
+            return None
+        for att in attributes:
+            if att['AttributeName'] == 'Verpackung':
+                package = package + att['AttributeValue'] + ', '
+        return (package)
+
+    # --------------------------- reformat_mouser_price --------------------------
+    # We need a Mouser specific modification to the price answer because they put
+    # funny things inside like an EURO sign and they use , instead of .
+    def reformat_mouser_price(self, price):
+        price = price.replace(',', '.')
+        non_decimal = re.compile(r'[^\d.]+')
+        price = float(non_decimal.sub('', price))
+        return price
+
+    # ------------------------ create_cart -------------------------------------------
+    # For Mouser this is just a dummy. We do not create a cart ID so far. It is
+    # automatically created by Mouser during item insertion. The return values are
+    # only for error handling.
+
+    def create_mouser_cart(self, order):
+        cart_data = {}
+        self.status_code = 200
+        cart_data['ID'] = ''
+        self.message = 'Success'
+        return (cart_data)
+
+    # ------------------------ update_cart ----------------------------------
+    # The Mouser part.
+    # Actually we send an empty CartKey. So Mouser creates a new key each time
+    # the button is pressed. This should be improved in future.
+
+    def update_mouser_cart(self, order, cart_key):
+        country_code = self.COUNTRY_CODES[InvenTreeSetting.get_setting('INVENTREE_DEFAULT_CURRENCY')]
+        cart_items = []
+        for item in order.lines.all():
+            cart_items.append({'MouserPartNumber': item.part.SKU,
+                               'Quantity': int(item.quantity),
+                               'CustomerPartNumber': item.part.part.IPN
+                               })
+        cart = {
+            "CartKey": cart_key,
+            "CartItems": cart_items
+        }
+        url = 'https://api.mouser.com/api/v001/cart/items/insert?apiKey=' + self.get_setting('MOUSERKEY') + '&countryCode=' + country_code
+        header = {'Content-type': 'application/json', 'Accept': 'application/json'}
+        response = Wrappers.post_request(self, json.dumps(cart), url, header)
+
+        # Return with error if response was not OK
+        if response.status_code != 200:
+            return ({})
+        response = response.json()
+        if response['Errors'] != []:
+            self.status_code = 'Mouser answered: '
+            self.message = response['Errors'][0]['Message']
+            return ({})
+        cart_items = []
+        for p in response['CartItems']:
+            if p['Errors'] == []:
+                cart_items.append({'SKU': p['MouserPartNumber'],
+                                   'IPN': p['CartItemCustPartNumber'],
+                                   'QuantityRequested': p['Quantity'],
+                                   'QuantityAvailable': p['MouserATS'],
+                                   'UnitPrice': p['UnitPrice'],
+                                   'ExtendedPrice': p['ExtendedPrice'],
+                                   'Error': ''
+                                   })
+            else:
+                cart_items.append({'SKU': p['MouserPartNumber'],
+                                   'IPN': p['CartItemCustPartNumber'],
+                                   'QuantityRequested': p['Quantity'],
+                                   'QuantityAvailable': p['MouserATS'],
+                                   'UnitPrice': p['UnitPrice'],
+                                   'ExtendedPrice': p['ExtendedPrice'],
+                                   'Error': p['Errors'][0]['Message']
+                                   })
+
+        # Here we get the currency_code from the Mouser response
+        shopping_cart = {'MerchandiseTotal': response['MerchandiseTotal'],
+                         'CartItems': cart_items,
+                         'cart_key': response['CartKey'],
+                         'currency_code': response['CurrencyCode'],
+                         }
+        self.status_code = 200
+        self.message = 'OK'
+        MetaAccess.set_value(self, order, self.NAME, 'MouserCartKey', response['CartKey'])
+        return (shopping_cart)
--- inventree_supplier_panel/supplier_panel.py
+++ inventree_supplier_panel/supplier_panel.py
@@ -10,6 +10,7 @@ from plugin.mixins import PanelMixin, SettingsMixin, UrlsMixin
 from company.models import Company, ManufacturerPart, SupplierPart
 from company.models import SupplierPriceBreak
 from inventree_supplier_panel.version import PLUGIN_VERSION
+from inventree_supplier_panel.mouser import Mouser
 from inventree_supplier_panel.meta_access import MetaAccess
 from inventree_supplier_panel.request_wrappers import Wrappers
 from users.models import check_user_role
@@ -17,7 +18,6 @@ from common.models import InvenTreeSetting
 
 from urllib.parse import quote
 import json
-import re
 
 
 class SupplierCartPanel(PanelMixin, SettingsMixin, InvenTreePlugin, UrlsMixin):
@@ -202,67 +202,6 @@ class SupplierCartPanel(PanelMixin, SettingsMixin, InvenTreePlugin, UrlsMixin):
             re_path(r'addsupplierpart(?:\.(?P<format>json))?$', self.add_supplierpart, name='add-supplierpart'),
         ]
 
-# ------------------------ update_cart ----------------------------------
-# The Mouser part.
-# Actually we send an empty CartKey. So Mouser creates a new key each time
-# the button is pressed. This should be improved in future.
-
-    def update_mouser_cart(self, order, cart_key):
-        country_code = self.COUNTRY_CODES[InvenTreeSetting.get_setting('INVENTREE_DEFAULT_CURRENCY')]
-        cart_items = []
-        for item in order.lines.all():
-            cart_items.append({'MouserPartNumber': item.part.SKU,
-                               'Quantity': int(item.quantity),
-                               'CustomerPartNumber': item.part.part.IPN
-                               })
-        cart = {
-            "CartKey": cart_key,
-            "CartItems": cart_items
-        }
-        url = 'https://api.mouser.com/api/v001/cart/items/insert?apiKey=' + self.get_setting('MOUSERKEY') + '&countryCode=' + country_code
-        header = {'Content-type': 'application/json', 'Accept': 'application/json'}
-        response = Wrappers.post_request(self, json.dumps(cart), url, header)
-
-        # Return with error if response was not OK
-        if response.status_code != 200:
-            return ({})
-        response = response.json()
-        if response['Errors'] != []:
-            self.status_code = 'Mouser answered: '
-            self.message = response['Errors'][0]['Message']
-            return ({})
-        cart_items = []
-        for p in response['CartItems']:
-            if p['Errors'] == []:
-                cart_items.append({'SKU': p['MouserPartNumber'],
-                                   'IPN': p['CartItemCustPartNumber'],
-                                   'QuantityRequested': p['Quantity'],
-                                   'QuantityAvailable': p['MouserATS'],
-                                   'UnitPrice': p['UnitPrice'],
-                                   'ExtendedPrice': p['ExtendedPrice'],
-                                   'Error': ''
-                                   })
-            else:
-                cart_items.append({'SKU': p['MouserPartNumber'],
-                                   'IPN': p['CartItemCustPartNumber'],
-                                   'QuantityRequested': p['Quantity'],
-                                   'QuantityAvailable': p['MouserATS'],
-                                   'UnitPrice': p['UnitPrice'],
-                                   'ExtendedPrice': p['ExtendedPrice'],
-                                   'Error': p['Errors'][0]['Message']
-                                   })
-
-        # Here we get the currency_code from the Mouser response
-        shopping_cart = {'MerchandiseTotal': response['MerchandiseTotal'],
-                         'CartItems': cart_items,
-                         'cart_key': response['CartKey'],
-                         'currency_code': response['CurrencyCode'],
-                         }
-        self.status_code = 200
-        self.message = 'OK'
-        MetaAccess.set_value(self, order, self.NAME, 'MouserCartKey', response['CartKey'])
-        return (shopping_cart)
-
 # --------------------------- get_partdata ------------------------------------
 # This is just the wrapper that selects the proper supplier dependant function
     def get_partdata(self, supplier, sku):
@@ -271,69 +210,6 @@ class SupplierCartPanel(PanelMixin, SettingsMixin, InvenTreePlugin, UrlsMixin):
             if supplier == self.registered_suppliers[s]['pk']:
                 part_data = self.registered_suppliers[s]['get_partdata'](self, sku)
         return (part_data)
-
-# --------------------------- get_mouser_partdata -----------------------------
-    def get_mouser_partdata(self, sku):
-
-        part_data = {}
-        part = {"SearchByPartRequest": {"mouserPartNumber": sku,
-                                        "partSearchOptions": "exact"
-                                        }
-                }
-        url = 'https://api.mouser.com/api/v1.0/search/partnumber?apiKey=' + self.get_setting('MOUSERSEARCHKEY')
-        header = {'Content-type': 'application/json', 'Accept': 'application/json'}
-        response = Wrappers.post_request(self, json.dumps(part), url, header)
-        if response.status_code != 200:
-            return (part_data)
-        response = response.json()
-        if response['Errors'] != []:
-            self.status_code = 'Error, '
-            self.message = response['Errors']
-            return (part_data)
-        number_of_results = int(response['SearchResults']['NumberOfResult'])
-        if number_of_results == 0:
-            self.status_code = 'Error, '
-            self.message = 'Part not found: ' + sku
-            return (part_data)
-        if number_of_results > 1:
-            self.status_code = 'Error, '
-            self.message = 'Multiple parts found. Check supplier part number: ' + sku
-            return (part_data)
-        part_data['SKU'] = response['SearchResults']['Parts'][0]['MouserPartNumber']
-        part_data['MPN'] = response['SearchResults']['Parts'][0]['ManufacturerPartNumber']
-        part_data['URL'] = response['SearchResults']['Parts'][0]['ProductDetailUrl']
-        part_data['lifecycle_status'] = response['SearchResults']['Parts'][0]['LifecycleStatus']
-        part_data['pack_quantity'] = response['SearchResults']['Parts'][0]['Mult']
-        part_data['description'] = response['SearchResults']['Parts'][0]['Description']
-        part_data['package'] = self.get_mouser_package(response['SearchResults']['Parts'][0])
-        part_data['price_breaks'] = []
-        for pb in response['SearchResults']['Parts'][0]['PriceBreaks']:
-            new_price = self.reformat_mouser_price(pb['Price'])
-            part_data['price_breaks'].append({'Quantity': pb['Quantity'], 'Price': new_price, 'Currency': pb['Currency']})
-        self.status_code = 200
-        self.message = 'OK'
-        return (part_data)
-
-    # We need a Mouser specific modification to the price answer because they put
-    # funny things inside like an EURO sign and they use , instead of .
-    def reformat_mouser_price(self, price):
-        price = price.replace(',', '.')
-        non_decimal = re.compile(r'[^\d.]+')
-        price = float(non_decimal.sub('', price))
-        return price
-
-# ------------------------------- get_mouser_package --------------------------
-# Extracts the available packages from the Mouser part data json
-    def get_mouser_package(self, part_data):
-        package = ''
-        try:
-            attributes = part_data['ProductAttributes']
-        except Exception:
-            return None
-        for att in attributes:
-            if att['AttributeName'] == 'Verpackung':
-                package = package + att['AttributeValue'] + ', '
-        return (package)
 
 # ------------------------------------------------------------------
 # The Digikey part
@@ -651,9 +527,9 @@ class SupplierCartPanel(PanelMixin, SettingsMixin, InvenTreePlugin, UrlsMixin):
                                        'name': 'Mouser',
                                        'po_template': 'supplier_panel/mouser.html',
                                        'is_registered': False,
-                                       'get_partdata': get_mouser_partdata,
-                                       'update_cart': update_mouser_cart,
-                                       'create_cart': create_mouser_cart,
+                                       'get_partdata': Mouser.get_mouser_partdata,
+                                       'update_cart': Mouser.update_mouser_cart,
+                                       'create_cart': Mouser.create_mouser_cart,
                                        },
                             'Digikey': {'pk': 0,
                                         'name': 'Digikey',
